2ff9c7dcaedd18b7f849af2be04c9468b3e44215
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-06-20 21:00:57 +0700

    TB config exporter stub

--- plugin.gd
+++ plugin.gd
@@ -2,11 +2,26 @@
 extends EditorPlugin
 
 
+const TOOL_MENU_NAME := "Export TrechBroom game config"
+var dialog: EditorFileDialog
+
+
 func _enter_tree():
-	# Initialization of the plugin goes here.
-	pass
+	add_tool_menu_item(TOOL_MENU_NAME, _export_tb_config)
+	
+	dialog = EditorFileDialog.new()
+	dialog.access = EditorFileDialog.ACCESS_FILESYSTEM
+	dialog.file_mode = EditorFileDialog.FILE_MODE_OPEN_DIR
+	dialog.dir_selected.connect(_write_tb_config)
 
 
 func _exit_tree():
-	# Clean-up of the plugin goes here.
-	pass
+	remove_tool_menu_item(TOOL_MENU_NAME)
+
+
+func _export_tb_config() -> void:
+	get_editor_interface().popup_dialog_centered(dialog, Vector2i(800, 600))
+
+
+func _write_tb_config(dir: String) -> void:
+	dialog.get_parent().remove_child(dialog)
