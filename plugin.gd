83b9073be4ac33219c2506061b663f0d3e89bbf6
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-06-21 20:56:51 +0700

    Downgrade to 4.0

--- plugin.gd
+++ plugin.gd
@@ -12,14 +12,11 @@ func _enter_tree():
 	dialog.file_mode = EditorFileDialog.FILE_MODE_SAVE_FILE
 	dialog.add_filter("*.cfg", "TrenchBroom game config")
 	
-	dialog.file_selected.connect(func(path: String):
-		_file_dialog_cleanup()
-		_export_tb_config(path)
-	)
-	dialog.canceled.connect(_file_dialog_cleanup)
+	dialog.file_selected.connect(_export_tb_config)
+	get_editor_interface().get_base_control().add_child(dialog)
 	
 	add_tool_menu_item(TOOL_MENU_NAME, func():
-		get_editor_interface().popup_dialog_centered(dialog, Vector2i(800, 600))
+		dialog.popup_centered(Vector2i(800, 600))
 		dialog.current_file = "GameConfig.cfg"
 	)
 
@@ -27,10 +24,6 @@ func _enter_tree():
 func _exit_tree():
 	remove_tool_menu_item(TOOL_MENU_NAME)
 	dialog.queue_free()
-
-
-func _file_dialog_cleanup() -> void:
-	dialog.get_parent().remove_child(dialog)
 
 
 func _export_tb_config(path: String) -> void:
